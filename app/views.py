1ef0b3be970991f60658477321ff4eb2f54221db
Addy Kim <soyaddyk@gmail.com>
2017-03-14 16:28:59 -0500

    Template api call method

--- app/views.py
+++ app/views.py
@@ -1,7 +1,14 @@
 from app import app
+import requests
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-	return ("Meow")
+	return 'Meow'
+
+SPOTIFY_API = 'https://api.spotify.com/v1/'
+
+TODO make this method useful
+def request_spotify()
+	r = requests.get(SPOTIFY_API)
+	return r.text
