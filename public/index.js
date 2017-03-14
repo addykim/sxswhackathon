16a70d63aea0ce84788cece65fd1aa5749b44409
Addy Kim <soyaddyk@gmail.com>
2017-03-14 15:26:42 -0500

    Add select dropdown

--- public/index.js
+++ public/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>Hello, world!sss</h1>,
-  document.getElementById('content')
-);
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <h1>What activity are you doing today?</h1>
+        <select>
+          <option value="yoga">Yoga</option>
+          <option value="weight-lifting">Weight lifting</option>
+          <option value="running">Running</option>
+        </select>
+      </div>
+      );
+  }
+}
+
+ReactDOM.render(<Index/>, document.getElementById('content'));
